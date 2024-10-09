054ef2ebd3cc3110544122c20636f78f82683f98
NALLURE <maldobai24@gmail.com>
2024-10-09 15:43:09 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
-// index.js
-// where your node app starts
 
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  
+
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+function isValidDate(date) {
+  return date instanceof Date && !isNaN(date);
+}
+
+// Endpoint to handle date requests
+app.get("/api/:date?", function (req, res) {
+  let inputDate = req.params.date;
+  let date;
+
+  if (!inputDate) {
+
+    date = new Date();
+  } else if (/^\d+$/.test(inputDate)) {
+
+    date = new Date(parseInt(inputDate));
+  } else {
+
+    date = new Date(inputDate);
+  }
+
+  if (!isValidDate(date)) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
 
-// Listen on port set in environment variable or default to 3000
+
+
+
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
